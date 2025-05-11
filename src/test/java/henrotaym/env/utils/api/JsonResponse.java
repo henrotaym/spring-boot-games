e2afe0061a5f19cb2704c0495db24a937598544b
henrotaym <mathieu.henrotay@gmail.com>
2025-05-11 17:32:43 +0200

    🚧 Json helpers to test api endpoints.

---
+++ src/test/java/henrotaym/env/utils/api/JsonResponse.java
@@ -0,0 +1,36 @@
+package henrotaym.env.utils.api;
+
+import java.util.function.Function;
+
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Component
+@RequiredArgsConstructor
+@Setter
+@Getter
+public class JsonResponse {
+    private ResultActions response;
+
+    public JsonResponse status(Function<StatusResultMatchers, ResultMatcher> callback) throws Exception {
+        this.response.andExpect(callback.apply(MockMvcResultMatchers.status()));
+
+        return this;
+    }
+
+    public JsonResponse contains(String expression, Function<JsonPathResultMatchers, ResultMatcher> callback) throws Exception {
+        this.response.andExpect(
+            callback.apply(MockMvcResultMatchers.jsonPath(expression))
+        );
+
+        return this;
+    }
+}
