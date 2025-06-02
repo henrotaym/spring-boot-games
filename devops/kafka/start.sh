ca7b8c1b6cab3314b6a7a007ede0a948087d2b65
henrotaym <mathieu.henrotay@gmail.com>
2025-06-02 16:33:46 +0200

    🔥 Remove unused data.

--- devops/kafka/start.sh
+++ devops/kafka/start.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-
-# cat <<EOF > /etc/kafka/server.properties.custom
-# node.id=${KAFKA_NODE_ID}
-# process.roles=${KAFKA_PROCESS_ROLES}
-# controller.quorum.voters=${KAFKA_CONTROLLER_QUORUM_VOTERS}
-# listeners=${KAFKA_LISTENERS}
-# advertised.listeners=${KAFKA_ADVERTISED_LISTENERS}
-# listener.security.protocol.map=${KAFKA_LISTENER_SECURITY_PROTOCOL_MAP}
-# controller.listener.names=${KAFKA_CONTROLLER_LISTENER_NAMES}
-# offsets.topic.replication.factor=${KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR}
-# EOF
       
 if [ ! -f /var/lib/kafka/data/meta.properties ]; then
     uuid=$(kafka-storage random-uuid)
