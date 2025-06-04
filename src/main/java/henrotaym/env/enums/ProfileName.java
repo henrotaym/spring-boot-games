07c6464162ab2dcd4935c170a885167a821a3bb5
henrotaym <mathieu.henrotay@gmail.com>
2025-06-04 18:53:28 +0000

    ✨ Scheduler.

--- src/main/java/henrotaym/env/enums/ProfileName.java
+++ src/main/java/henrotaym/env/enums/ProfileName.java
@@ -4,4 +4,5 @@ public class ProfileName {
   public static final String TEST = "test";
   public static final String QUEUE = "queue";
   public static final String HTTP = "http";
+  public static final String SCHEDULER = "scheduler";
 }
